ce616a5c49577da7b916217ba2cdf963904681a8
silentarrowz <silentarrowz@gmail.com>
2017-08-18 13:24:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+
+          <h2>Welcome to Simple Redux Counter</h2>
+        </div>
+        <p className="App-intro">
+          <button onClick={this.props.increment} >+</button>
+          <button onClick={this.props.decrement} >-</button><br/>
+        Present Value is {this.props.value}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
